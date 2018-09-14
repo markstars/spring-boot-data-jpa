e13585302801b4e91ec5841ebc737b61d422e111
markstars <1291324610@qq.com>
2018-09-14 23:04:55 +0800

    Initial commit

---
+++ src/main/java/com/mao/springbootdatajpa/SpringBootDataJpaApplication.java
@@ -0,0 +1,12 @@
+package com.mao.springbootdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataJpaApplication.class, args);
+    }
+}
---
+++ src/main/java/com/mao/springbootdatajpa/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.mao.springbootdatajpa.controller;
+
+
+import com.mao.springbootdatajpa.entity.User;
+import com.mao.springbootdatajpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/user/{id}")
+    public Optional<User> getUser(@PathVariable("id") Integer id){
+        Optional<User> user = userRepository.findById(id);
+        return user;
+    }
+
+    @GetMapping("/user")
+    public User insertUser(User user){
+        User save = userRepository.save(user);
+        return save;
+    }
+
+}
